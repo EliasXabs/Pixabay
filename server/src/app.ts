34bc159118f514d2ac16d85817e1910cdbd8ba2f
@EliasXabs <eliasxabs@gmail.com>
2024-08-01 11:44:23 +0300

    env setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
